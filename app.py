308fdf0faf9cf4503b8318fc83ea41741ceeaa37
C0Aditya <theodricknight0@gmail.com>
2025-06-26 23:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if st.button("Send") and user_input:
 
     try:
         res = requests.post(
-            "https://appointment-booking-chatbot-yu7qxdcmhynnddftuzejq9.streamlit.app/",
+            "http://localhost:8000/chat,
             json={"user_id": st.session_state.user_id, "message": user_input}
         )
 
